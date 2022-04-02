9f1e76cdf2cac08c43e606c9aaae0db1af8348e9
Vest <Vest@users.noreply.github.com>
2022-04-02 21:44:26 +0200

    Draft solution of Problem 12
    Signed-off-by: Vest <Vest@users.noreply.github.com>

--- src/problem12.rs
+++ src/problem12.rs
@@ -1,53 +1,39 @@
+use primes::{factors, factors_uniq};
+
 fn get_triangle_number(n: usize) -> usize {
-    (1..=n).sum()
+    (1 + n) * n / 2
 }
 
 fn count_divisor(n: usize) -> usize {
-    let is_even = n % 2 == 0;
-    let max = if is_even { n / 2 } else { (n + 1) / 2 };
-    (1..max)
-        .map(|d| n % d)
-        .filter(|&r| r == 0_usize)
-        .take(500)
-        .count() + if is_even { 2 } else { 1 }
+    let divisors = factors(n as u64);
+    let unique_divisors = factors_uniq(n as u64);
+    let count_divisors = divisors.len();
+    let count_unique_divisors = unique_divisors.len();
+
+    dbg!(n, divisors, unique_divisors, count_divisors);
+
+    (1..count_unique_divisors)
+        .map(|c| count_comb(count_divisors + c - 1, c))
+        .sum::<usize>() + if n == 1 { 1 } else { 2 }
+}
+
+fn factorial(n: usize) -> usize {
+    (1..n)
+        .product()
+}
+
+fn count_comb(n: usize, k: usize) -> usize {
+    (n - k + 1..=n).product::<usize>() / factorial(k)
 }
 
 fn main() {
-    let mut begin = 1;
-    let mut end = 2;
-
-    let ans_begin =  count_divisor(get_triangle_number(1000000));
-    println!("Problem 12. Answer is {}", ans_begin);
-/*
-    loop {
-        let ans_begin =  count_divisor(get_triangle_number(begin));
-        let ans_end =  count_divisor(get_triangle_number(end));
-
-        if ans_end < 500 {
-            begin = end;
-            end *= 10;
-        } else {
-            println!("Found end {}", end)
-        }
-        if ans_begin < 500 {
-          //  begin += 1;
-        } else {
-            println!("Problem 12. Answer is {}", begin);
-            break;
-        }
-    }
-
- */
-/*
     let answer = (1..)
         .map(get_triangle_number)
         .map(|t| (t, count_divisor(t)))
-        .filter(|&(_, count)| count >= 500_usize)
+        .filter(|&(_, count)| dbg!(count) >= 500_usize)
         .map(|(t, _)| t)
         .next();
     println!("Problem 12. Answer is {}", answer.unwrap_or_default());
-
- */
 }
 
 #[cfg(test)]
